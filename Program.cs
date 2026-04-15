3c0536c8cdd77328c76c7d013b9334a1d79a48a6
edgar-tech09 <edgarcamujr2000@gmail.com>
2026-04-16 03:54:39 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-submission setup
+using System;
+
+namespace Camu_EdgarJr_ShoppingCartActivity
+{
+
+    class Ukayproduct
+    {
+        public int ID;
+        public string BrandName;
+        public double Price;
+        public int Stock;
+
+        public void DisplayProduct()
+        {
+
+            Console.WriteLine(ID + " . " + BrandName.PadRight(30) + " | PHP " + Price + " | " + Stock + " PCS");
+        }
+
+        public double GetSubtotal(int quantity)
+        {
+            return Price * quantity;
+        }
+    }
+
+
+}
